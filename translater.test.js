3eb8a288221d124315af1a8f646d5cccb625187c
Khemi <khemi@Khemis-Laptop.local>
2021-11-22 13:00:38 +0000

    iteration2: switch case for letters to morse

--- translater.test.js
+++ translater.test.js
@@ -1,8 +1,78 @@
-import {expect, it} from "@jest/globals";
-import {translate} from "./translate.js"
+import {expect, it, describe} from "@jest/globals";
+import {translateEnglishToMorse} from "./translate.js"
 
-//5
-it("should translate hello to bonjour", ()=>{
-    const result = translate("bonjour");
-    expect(result).toBe("hello");
+describe ("Translate single letters to morse", () => {
+//5 valid tests (letters to morse)
+it("should translate a to .-", ()=>{
+    const result = translateEnglishToMorse("a");
+    expect(result).toBe(".-");
+})
+
+it("should translate b to -...", ()=>{
+    const result = translateEnglishToMorse("b");
+    expect(result).toBe("-...");
+})
+
+it("should translate c to -.-.", ()=>{
+    const result = translateEnglishToMorse("c");
+    expect(result).toBe("-.-.");
+})
+
+it("should translate d to -..", ()=>{
+    const result = translateEnglishToMorse("d");
+    expect(result).toBe("-..");
+})
+
+it("should translate e to .", ()=>{
+    const result = translateEnglishToMorse("e");
+    expect(result).toBe(".");
+})
+
+//5 valid tests (words to morse)
+it("should translate hi to ......", ()=>{
+    const result = translateEnglishToMorse("hi");
+    expect(result).toBe("......");
+})
+/*
+//5 valid tests (morse to letters)
+it("should translate ..-. to f", ()=>{
+    const result = translate("..-.");
+    expect(result).toBe("f");
+})
+
+it("should translate --. to g", ()=>{
+    const result = translate("--.");
+    expect(result).toBe("g");
+})
+
+it("should translate .... to h", ()=>{
+    const result = translate("....");
+    expect(result).toBe("h");
+})
+
+it("should translate .. to i", ()=>{
+    const result = translate("..");
+    expect(result).toBe("i");
+})
+
+it("should translate e to .", ()=>{
+    const result = translate("e");
+    expect(result).toBe(".");
+})*/
+
+//3 invalid tests
+it("should translate ! to invalid", ()=>{
+    const result = translate("!");
+    expect(result).toBe("invalid");
+})
+
+it("should translate $ to invalid", ()=>{
+    const result = translate("$");
+    expect(result).toBe("invalid");
+})
+
+it("should translate % to invalid", ()=>{
+    const result = translate("%");
+    expect(result).toBe("invalid");
+})
 })
