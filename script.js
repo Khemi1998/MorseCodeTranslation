a79269845c1f3300b1e700908375482a05802a25
Khemi <khemi@Khemis-Laptop.local>
2021-11-22 18:24:38 +0000

    iteration11: added reset

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ import {translateEnglishToMorse} from "./translate.js"
 const output = document.querySelector(".toMorse__output");
 const input = document.querySelector(".toMorse__text");
 const button = document.querySelector(".toMorse__buttons");
+const restart = document.querySelector(".toMorse__reset");
 
 //inputting into translateEnglishToMorse 
 //recieing an output
 console.log("input.value")
 button.addEventListener("click", () => {
     const inputText = input.value;
+    if (inputText.length>0) {
     const morseTranslation = translateEnglishToMorse(inputText);
     output.innerHTML=`${morseTranslation}`;
+    }
   })
+
+restart.addEventListener("click", ()=>{
+   input.value = "";
+   output.innerHTML= `Morse-code will be displayed here`
+})
