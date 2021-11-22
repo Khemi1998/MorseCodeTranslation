25e8e97d8b7b2aacf4037c46fa284a690fc20288
Khemi <khemi@Khemis-Laptop.local>
2021-11-22 16:58:09 +0000

    iteration9: updating style

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const output = document.querySelector(".toMorse__output");
 const input = document.querySelector(".toMorse__text");
 const button = document.querySelector(".toMorse__buttons");
 
+//inputting into translateEnglishToMorse 
+//recieing an output
 console.log("input.value")
 button.addEventListener("click", () => {
     const inputText = input.value;
